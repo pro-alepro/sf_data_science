582ef0a2d949a1216e0992f209239c05560d3b11
pro-alepro <alepro@bk.ru>
2022-03-24 23:10:15 +0300

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,43 @@
+"""Игра угадай число"""
+"""Компьютер сам угадывает загаданное число"""
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """_summary_
+
+    Args:
+        number (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        int: _description_
+    """
+    count = 0
+    while True:
+        count+=1
+        predict_number = np.random.randint(1,101)
+        if number == predict_number:
+            break
+    return count
+
+def score_game(random_predict) -> int:
+    """_summary_
+
+    Args:
+        random_predict (_type_): _description_
+
+    Returns:
+        int: _description_
+    """
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1,101,size=1000)
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает в среднем за {score} попыток")
+    return(score)
+
+if __name__ == '__main__':
+    score_game(random_predict)
